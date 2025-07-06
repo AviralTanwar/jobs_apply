a2a7250726423c173142eaa821cfba39b4ae17c0
Aviral Tanwar <aviraltanwar@gmail.com>
2025-07-06 17:43:14 +0530

    Trying to use Selenium rather than BeautifulSOup with headlee browser

--- scripts/Automation/linkedIn/surf_linkedin.py
+++ scripts/Automation/linkedIn/surf_linkedin.py
@@ -1,7 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-import os
-import json
+
 
 def fetch_linkedin_job_ids(url):
     """
@@ -13,46 +10,8 @@ def fetch_linkedin_job_ids(url):
         if "linkedin" not in url:
             raise ValueError("URL is not a LinkedIn job URL")
 
-        response = requests.get(url)
-        response.raise_for_status()
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        print("--------------------------------------------------------------------------") 
-        print("-----------------------------soup---------------------------------------------")
-        print("--------------------------------------------------------------------------") 
-        print(soup.prettify())  # Print the HTML content for debugging
-        print("--------------------------------------------------------------------------") 
-
-        print("")
-        print("")
-        print("")
-        print("")
-        print("")
-
-        # Find the job container using LinkedIn's UI class
-        job_elements = soup.find_all("li", attrs={"data-occludable-job-id": True})
-        print("--------------------------------------------------------------------------") 
-        print("-----------------------------job_elements---------------------------------------------")
-        print("--------------------------------------------------------------------------") 
-        print(job_elements)  # Print the HTML content for debugging
-        print("--------------------------------------------------------------------------") 
-
-        print("")
-        print("")
-        print("")
-        print("")
-        print("")
-
-        job_dict = {}
-        for job_elem in job_elements:
-            job_id = job_elem.get("data-occludable-job-id")
-            if job_id:
-                job_dict[job_id] = {}  # Empty dictionary for now
-
-        print("The Dictionary of LinkedIn jobs is:")
-        print(json.dumps(job_dict, indent=4))
-
-        return job_dict
+        return None
 
     except Exception as e:
         print(f"An error occurred in the function fetch_linkedin_job_ids: {e}")
